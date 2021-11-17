1bd47811890d175d6bf36138c4def716ba22a161
Gardener Merlene <gmerlene@il-f1.msk.21-school.ru>
2021-11-17 19:02:18 +0300

    Fix error, when red cross is pressed

--- bonus/init_hooks_bonus.c
+++ bonus/init_hooks_bonus.c
@@ -6,7 +6,7 @@
 /*   By: gmerlene <gmerlene@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 17:56:16 by gmerlene          #+#    #+#             */
-/*   Updated: 2021/11/17 16:14:02 by gmerlene         ###   ########.fr       */
+/*   Updated: 2021/11/17 18:56:21 by gmerlene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,16 @@ static int	handle_key_press(int keycode, t_game *game)
 	return (0);
 }
 
+static int	handle_cross_press(t_game *game)
+{
+	exit_game(game, NULL);
+	return (0);
+}
+
 void	init_hooks(t_game *game)
 {
 	mlx_key_hook(game->win, handle_key_press, game);
-	mlx_hook(game->win, 17, 0, exit_game, game);
+	mlx_hook(game->win, 17, 0, handle_cross_press, game);
 	render_window(game);
 	print_n_movemets(game);
 	mlx_loop_hook(game->mlx, animate_enemies, game);
